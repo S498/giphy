c842e7a2a8328ab1480e93b4eed4fc3df70921d2
Sathwik Edupuganti <sathwik.edupuganti@gmail.com>
2020-06-03 13:32:47 +0530

    Updates

---
+++ precache-manifest.df705737783b6555b6ac2225f7490b6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db5368d0458f9af73242512a12627666",
+    "url": "/giphy/public/index.html/index.html"
+  },
+  {
+    "revision": "3c8cbecc649d0c2aaa86",
+    "url": "/giphy/public/index.html/static/css/2.3c515868.chunk.css"
+  },
+  {
+    "revision": "482e60b1a9436d10cb81",
+    "url": "/giphy/public/index.html/static/css/main.68ecc08e.chunk.css"
+  },
+  {
+    "revision": "3c8cbecc649d0c2aaa86",
+    "url": "/giphy/public/index.html/static/js/2.d014e12f.chunk.js"
+  },
+  {
+    "revision": "5fb8706fb48eab2465654dcef48581dc",
+    "url": "/giphy/public/index.html/static/js/2.d014e12f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "482e60b1a9436d10cb81",
+    "url": "/giphy/public/index.html/static/js/main.a1cd0713.chunk.js"
+  },
+  {
+    "revision": "aca5dc558c59d0bfb096",
+    "url": "/giphy/public/index.html/static/js/runtime-main.45d0bed2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/giphy/public/precache-manifest.026bf1b589160e7cf79b36794fb8a8b7.js"
+  "/giphy/public/index.html/precache-manifest.df705737783b6555b6ac2225f7490b6c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/giphy/public/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/giphy/public/index.html/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
